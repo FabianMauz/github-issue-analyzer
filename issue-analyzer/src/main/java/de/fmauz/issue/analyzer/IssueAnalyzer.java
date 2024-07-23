369f38817331cccfae1309ff793e26c2e7443c4d
fmauz <fmauz@ipb-halle.de>
2024-07-23 11:28:13 +0200

    initial commit

---
+++ issue-analyzer/src/main/java/de/fmauz/issue/analyzer/IssueAnalyzer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package de.fmauz.issue.analyzer;
+
+/**
+ *
+ * @author fmauz
+ */
+public class IssueAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
